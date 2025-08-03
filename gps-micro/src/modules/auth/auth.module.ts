a8f464ac92eaa65b29dbfdb7e83a3740e5c64682
nnminh-haise <nnminh.sam.1803@gmail.com>
2025-08-04 00:39:20 +0700

    update: cleaned unsued modules

--- gps-micro/src/modules/auth/auth.module.ts
+++ gps-micro/src/modules/auth/auth.module.ts
@@ -1,32 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
-import { JwtModule } from '@nestjs/jwt';
-import { ConfigModule, ConfigService } from '@nestjs/config';
-import { EnvSchema } from 'src/config';
-import { UserModule } from 'src/modules/user/user.module';
-import { RoleModule } from 'src/modules/role/role.module';
 import { ClientModule } from 'src/client/client.module';
-import { AuditLogModule } from 'src/modules/audit-log/audit-log.module';
 
 @Module({
-  imports: [
-    AuditLogModule,
-    UserModule,
-    RoleModule,
-    JwtModule.registerAsync({
-      imports: [ConfigModule],
-      inject: [ConfigService],
-      useFactory: (configService: ConfigService<EnvSchema>) => {
-        const secret = configService.get<string>('JWT_SECRET');
-        return {
-          secret,
-          signOptions: { expiresIn: '1h' },
-        };
-      },
-    }),
-    ClientModule,
-  ],
+  imports: [ClientModule],
   controllers: [AuthController],
   providers: [AuthService],
 })
